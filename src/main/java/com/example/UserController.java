39054865583941e2a99998f3b004a0347c9cc5e1
sebastian.bogado <sebastian.bogado@fdvsolutions.com>
2013-10-15 05:13:49 -0300

    Testing Grails-like controller

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -8,7 +8,9 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.GET;
@@ -27,7 +29,13 @@ public class UserController {
 	public UserController(){
 	    users = new HashMap<String, User>();
 	    users.put("admin", new User(1,"Admin","PAMPITA"));
-	    users.put("common", new User(2,"Common","MarioBros"));
+	    users.put("common1", new User(2,"Common","MarioBros"));
+	    users.put("common2", new User(3,"Common","Juan Carlos Pelotudo"));
+	    users.put("common3", new User(4,"Common","Luis Almirante Brown"));
+	    users.put("common4", new User(5,"Common","Jesús de Laferrere"));
+	    users.put("common5", new User(6,"Common","Ministro de Ahorro Postal"));
+	    users.put("common6", new User(7,"Common","Mimo Páez"));
+	    users.put("common7", new User(8,"Common","Quiste Sebáceo"));
 	}
 	
     @GET
@@ -85,5 +93,13 @@ public class UserController {
         return "laaaaaaallaaa";
     }
 
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> list(@QueryParam("max") Integer max) {
+        Integer qty = max != null? max : 5;
+        List< User > list = new ArrayList< User >(users.values());
+        return list.subList(0, qty);
+    }
 
 }
