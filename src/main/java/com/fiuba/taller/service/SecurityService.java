dc1641f812fb3bdc941112174ef03ce16c73a1fe
AleMarra <alemarra@gmail.com>
2013-11-05 17:47:14 -0300

    Fixed excepcion cuando el logeo era inválido

--- src/main/java/com/fiuba/taller/service/SecurityService.java
+++ src/main/java/com/fiuba/taller/service/SecurityService.java
@@ -71,7 +71,7 @@ public class SecurityService {
 	    }else{
 	    	return Response
 	                .status(response.getStatus())
-	                .type(securityResponse.getReason())
+	                .entity(securityResponse.getReason())
 	                .build();
 	    }
     }
