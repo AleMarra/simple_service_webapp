921031ce5c4d75ec355aaf0f70856083388fb95e
AleMarra <alemarra@gmail.com>
2013-11-07 16:09:45 -0300

    Ejemplos básicos de como usar la parametrizacion. Falta agregar parseo de xml o yaml y generar una suite completa

--- src/test/java/parameterized/RESTApiTestCase.java
+++ src/test/java/parameterized/RESTApiTestCase.java
@@ -1,53 +1,22 @@
 package parameterized;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-
-import org.junit.Test;
-import org.junit.runners.Parameterized.Parameters;
+import javax.ws.rs.core.Response;
 
 public class RESTApiTestCase {
 	
-	static protected Client client = ClientBuilder.newClient();
-	static protected String baseUrl;
-	static protected WebTarget webTarget;
+	protected String baseUrl = "";
+	protected WebTarget webTarget = null;
 	
-	static protected void setBaseTarget(String url){
+	protected Response response = null;
+	
+	protected void setBaseTarget(String url){
 		baseUrl = url;
-		webTarget = client.target(baseUrl);
-	}
-	
-	static protected class Step{
-		public String actionType;		  // HTTP action type: GET, POST, PUT, DELETE
-		public String actionUrl;		  // url from base, ie: /controller/method
-		public List<HashMap<String, String>> urlParams;   // Mandatory action params, ie: /controller/method/id
-		public List<HashMap<String, String>> queryParams; // Optional action params, ie: /controller/method/id?opt=val
-		
-		public String expectedResult;
-	
-		public Step(String actionType, String actionUrl,
-				List<HashMap<String, String>> urlParams,
-				List<HashMap<String, String>> queryParams,
-				String expectedResult) 
-		{
-		
-		this.actionType = actionType;
-		this.actionUrl = actionUrl;
-		this.urlParams = urlParams;
-		this.queryParams = queryParams;
-		this.expectedResult = expectedResult;
-		
-		}
+		webTarget = ClientBuilder.newClient().target(baseUrl);
 	}
 	
 	protected List<Step> steps;
--- src/test/java/parameterized/SecurityServiceTest.java
+++ src/test/java/parameterized/SecurityServiceTest.java
@@ -7,11 +7,14 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,24 +25,10 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 
-import parameterized.RESTApiTestCase.Step;
 
 @RunWith(Parameterized.class)
 public class SecurityServiceTest extends RESTApiTestCase{
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
-		setBaseTarget("http://localhost:8080/simple-service-webapp/api");
-	}
-
-	/*
-	 * Called once for every @Parameter in generateData result list 	
-	 */
-	public SecurityServiceTest(ArrayList<Step> steps){
-		this.steps = steps;
-	}
-	
 	/*
 	 * Test data generator.
 	 * This method is called the the JUnit parameterized test runner and
@@ -49,35 +38,46 @@ public class SecurityServiceTest extends RESTApiTestCase{
 	@Parameters
 	public static Collection<Object[]> generateData()
 	{
-		List<HashMap<String, String>> urlParams = new ArrayList<HashMap<String, String>>();
-		HashMap<String, String> url_param = new HashMap<String, String>();  
-		url_param.put("id", "1");
-		urlParams.add(url_param);
+		LinkedHashMap<String, String> urlParams = new LinkedHashMap<String, String>();
+		urlParams.put("id", "1");
+
+		LinkedHashMap<String, String> queryParams = new LinkedHashMap<String, String>();
+		queryParams.put("keepLogged", "false");
 		
-		List<HashMap<String, String>> queryParams = new ArrayList<HashMap<String, String>>();
-		HashMap<String, String> query_param = new HashMap<String, String>();
-		query_param.put("keepLogged", "false");
-		queryParams.add(query_param);
+		MultivaluedMap<String, String> formEntity = new MultivaluedHashMap<String, String>();
+		//{apellido=[a], nombre=[a], username=[a], email=[sdasd], fechaNac=[2013-01-01], padron=[11], rol=[1], password=[a]}
+		formEntity.addFirst("username", "test");
+		formEntity.addFirst("password", "test");
 	
 		ArrayList<Step> firstRun = new ArrayList<Step>();
-		firstRun.add(new Step("GET",
-							"/login",
-							urlParams,
-							queryParams,
+		firstRun.add(new Step("POST",
+							"login",
+							MediaType.APPLICATION_JSON,
+							formEntity,
+							new LinkedHashMap<String, String>(),
+							new LinkedHashMap<String, String>(),
+							200,
 							"{\"API\": \"login working\"}"));
 		
-		firstRun.add(new Step("POST",
-						"/login",
-						urlParams,
-						queryParams,
-						"{\"API\": \"login working\"}"));
+		ArrayList<Step> secondRun = new ArrayList<Step>();
+		secondRun.add(new Step("POST",
+							"login",
+							MediaType.APPLICATION_JSON,
+							formEntity,
+							null,
+							null,
+							200,
+							"{\"API\": \"login working\"}"));
 
 		
-		ArrayList<Step> secondRun = new ArrayList<Step>();
-		firstRun.add(new Step("GET",
-							"/logout",
-							urlParams,
-							new ArrayList<HashMap<String, String>>(),
+		ArrayList<Step> thirdRun = new ArrayList<Step>();
+		thirdRun.add(new Step("POST",
+							"logout",
+							MediaType.APPLICATION_JSON,
+							formEntity,
+							new LinkedHashMap<String, String>(),
+							new LinkedHashMap<String, String>(),
+							200,
 							"{\"API\": \"logout working\"}"));
 				
 		return Arrays.asList(new Object[][]{
@@ -87,6 +87,19 @@ public class SecurityServiceTest extends RESTApiTestCase{
 
 	}
 	
+	/*
+	 * Called once for every @Parameter in generateData result list 	
+	 */
+	public SecurityServiceTest(ArrayList<Step> steps){
+		this.steps = steps;
+	}
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		// Called once per step
+		setBaseTarget("http://localhost:8080/simple-service-webapp/api/");
+	}
 	
 	@Test
 	public void testRequestReturnsAsExpected() {
@@ -94,11 +107,53 @@ public class SecurityServiceTest extends RESTApiTestCase{
 		
 		while (it.hasNext()){
 			Step step = it.next();
-			System.out.println("Dummy test url: "+ baseUrl + step.actionUrl);
-			System.out.println(step.actionType +" "+
-							step.urlParams.toString() +" "+
-							step.queryParams.toString()+" "+
-							step.expectedResult);
+			
+			System.out.println(step.toString());
+			
+			webTarget = webTarget.path(step.actionUrl);
+			
+			if (step.urlParams != null){
+				for(Entry<String, String> requiredParam: step.urlParams.entrySet()){
+					// Adds url params in the form of /key/value
+					// LinkedHashMap guarantees iteration order to be the same as input order
+					System.out.println(requiredParam.toString());
+					webTarget = webTarget.path(requiredParam.getKey())
+							 .path(requiredParam.getValue());
+				}
+			}
+			
+			if (step.queryParams != null){
+				for(Entry<String, String> optionalParam: step.queryParams.entrySet()){
+					// Adds query params in the form of ?key=value
+					System.out.println(optionalParam.toString());
+					webTarget = webTarget.queryParam(optionalParam.getKey(), optionalParam.getValue());
+				}
+			}
+			
+			
+			switch (step.actionType){
+			case "GET": 
+						response = webTarget.request(step.mediaType).get();
+						break;
+						
+			case "POST":
+						response = webTarget.request(step.mediaType).post(Entity.form(step.formEntity));
+						break;
+						
+			case "PUT": 
+						break;
+						
+			case "DELETE": 
+						break;
+			}			
+			
+			
+			System.out.println(webTarget.getUri().toString());
+		
+			
+			assertTrue("Expected " +step.status+", got "+response.toString(),response.getStatus() == step.status);
+			
+			
 			
 			assertTrue("Dummy test: " + step.actionType + step.actionUrl, true);
 		}
