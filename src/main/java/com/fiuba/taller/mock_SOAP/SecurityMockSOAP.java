e54425e58301a49ca83f84b528b6b9ec4b953698
AleMarra <alemarra@gmail.com>
2013-11-15 00:32:52 -0300

    Basic soap server for security mock. No soap client yet

--- src/main/java/com/fiuba/taller/mock_SOAP/SecurityMockSOAP.java
+++ src/main/java/com/fiuba/taller/mock_SOAP/SecurityMockSOAP.java
@@ -1,20 +1,66 @@
 package com.fiuba.taller.mock_SOAP;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-import javax.ws.rs.FormParam;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
-@WebService
-public interface SecurityMockSOAP {
+import com.fiuba.taller.mock.UserDB;
+import com.fiuba.taller.service.SecurityResponse;
 
-	@WebMethod
-	public Response registerUser(MultivaluedMap<String, String> formParams);
+public class SecurityMockSOAP {
+
+	 static private UserDB users = new UserDB();
+	 static private String baseAuthToken = "bad18eba1ff45jk7858b8ae88a77fa30";
+	 static private Map<String, String> authenticatedUsers = new HashMap<String, String>();
+
+	    
+	public String ping(String text) {
+	    if (text == null) {
+	      return "TEST";
+	    }
+	    return "Service is up and available, message: " + text;
+	  }
 	
-	@WebMethod
-	public Response login(@FormParam("username") String username,
-			 			   @FormParam("password") String password);
-	@WebMethod
-	public Response logout(@FormParam("authToken") String authToken);
+	public SecurityResponse registerUser(MultivaluedMap<String, String> formParams) {
+		
+		if (formParams != null) {
+			try{
+				Map<String, String> userData = new HashMap<String, String>();
+			
+				Iterator it = formParams.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry pair = (Map.Entry) it.next();
+					String key = (String) pair.getKey();
+					List<String> value = (List<String>) pair.getValue();
+					userData.put(key, value.get(0));
+				}
+	
+				SecurityResponse securityEntity = null;
+				if (users.createUser(userData)) {
+					securityEntity = new SecurityResponse(true,
+							"Usuario creado exitosamente");
+				} else {
+					securityEntity = new SecurityResponse(false,
+							"El usuario ya existe");
+				}
+	
+				//return Response.status(200).entity(securityEntity).build();
+	
+				return securityEntity;
+			
+			}catch(Exception e){
+				e.printStackTrace();
+				return new SecurityResponse(false, "Datos inválidos con excepcion");
+			}
+		
+		} else {
+			
+			return new SecurityResponse(false, "Datos inválidos");
+		}
+    }
+
 }
--- src/main/java/com/fiuba/taller/service/SecurityService.java
+++ src/main/java/com/fiuba/taller/service/SecurityService.java
@@ -1,5 +1,26 @@
 package com.fiuba.taller.service;
 
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -7,161 +28,221 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
-
-import javax.ws.rs.*;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.*;
-import javax.ws.rs.core.Response.ResponseBuilder;
+import com.fiuba.taller.mock_SOAP.SoapProvider;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.client.filter.LoggingFilter;
+//import javax.ws.rs.client.Client;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class SecurityService {
 
-    static private String securityApiUrl = "http://localhost:8080/simple-service-webapp/api/mocks/";
-    static private Client client = ClientBuilder.newClient();
-    static private WebTarget webTarget = client.target(securityApiUrl);
+//    static private String securityApiUrl = "http://localhost:8080/simple-service-webapp/api/mocks/";
+//    static private Client client = ClientBuilder.newClient();
+//    static private WebTarget webTarget = client.target(securityApiUrl);
+//    
+    static private String securityApiUrl_SOAP = "http://localhost:8080/simple-service-webapp/services/SecurityMockSOAP";
+//    static private Client client_SOAP = ClientBuilder.newClient();
+//    static private WebTarget webTarget_SOAP = client_SOAP.target(securityApiUrl_SOAP);
 
-
-    @POST
-    @Path("registeruser")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response registerUser(MultivaluedMap<String, String> formParams) {
-        System.out.println(formParams);
-        WebTarget resourceWebTarget = webTarget.path("registeruser");
-        
-        Response response = resourceWebTarget
-				                .request(MediaType.APPLICATION_XML_TYPE)
-				                .post(Entity.form(formParams));
-        
-        response.bufferEntity();
-
-        System.out.println(response.toString());
-        
-        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
-
-        System.out.println(securityResponse.toString());
-
-        if(securityResponse.getSuccess()){
-            return Response
-                    .ok()
-                    .build();
-        }else{
-            return Response
-                    .status(response.getStatus())
-                    .entity(securityResponse)
-                    .build();
-        }
-    }
-
-    @POST
-    @Path("login")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response login(MultivaluedMap<String, String> formParams) {
-        WebTarget resourceWebTarget = webTarget.path("login");
-        Response response = resourceWebTarget
-                .request(MediaType.APPLICATION_XML_TYPE)
-                .post(Entity.form(formParams));
-
-        response.bufferEntity();
-        
-        System.out.println(response.toString());
-        
-        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
-        
-        if(securityResponse.getSuccess()){
-	        return Response
-	                .ok()
-	                .cookie(new NewCookie("authToken", securityResponse.getAuthToken()))
-	                .build();
-	    }else{
-	    	return Response
-	                .status(response.getStatus())
-	                .entity(securityResponse)
-	                .build();
-	    }
-    }
-
-    @POST
-    @Path("logout")
-    public Response logout(@CookieParam("authToken") String authToken) {
-        System.out.println("@CookieParam: " + authToken);
-        Form form = new Form();
-        form.param("authToken", authToken);
-
-        WebTarget resourceWebTarget = webTarget.path("logout");
-        Response response = resourceWebTarget
-                .request(MediaType.APPLICATION_XML_TYPE)
-                .post(Entity.form(form));
-
-        response.bufferEntity();
-        
-        System.out.println(response.toString());
-        
-        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
-        
-        if(securityResponse.getSuccess()){
-	        return Response
-	                .ok()
-	                .header("Set-Cookie",
-                            "authToken=deleted;Expires=Thu, 01-Jan-1970 00:00:01 GMT")
-	                .build();
-        }else{
-        	return Response
-	                .status(response.getStatus())
-                    .entity(securityResponse)
-	                .build();
-        }
-    }
-
-    @POST
-    @Path("isloggedin")
-    public Response isLoggedIn(@CookieParam("authToken") String authToken) {
-        SecurityResponse securityResponse;
-
-        if (authToken == null) {
-            securityResponse = new SecurityResponse(false, "No token");
-            return Response
-                    .ok()
-                    .entity(securityResponse)
-                    .build();
-        } else {
-            System.out.println("@CookieParam: " + authToken);
-            Form form = new Form();
-            form.param("authToken", authToken);
-
-            WebTarget resourceWebTarget = webTarget.path("isvalidtoken");
-            Response response = resourceWebTarget
-                    .request(MediaType.APPLICATION_XML_TYPE)
-                    .post(Entity.form(form));
-
-            response.bufferEntity();
-
-            System.out.println(response.toString());
-        
-            securityResponse = response.readEntity(SecurityResponse.class);
-
-
-            if(securityResponse.getSuccess()){
-                return Response
-                        .ok()
-                        .build();
-            } else {
-                return Response
-                        .status(response.getStatus())
-                        .entity(securityResponse)
-                        .build();
-            }
-        }
-    }
+//
+//    @POST
+//    @Path("registeruser")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    public Response registerUser(MultivaluedMap<String, String> formParams) {
+//        System.out.println(formParams);
+//        WebTarget resourceWebTarget = webTarget.path("registeruser");
+//        
+//        Response response = resourceWebTarget
+//				                .request(MediaType.APPLICATION_XML_TYPE)
+//				                .post(Entity.form(formParams));
+//        
+//        response.bufferEntity();
+//
+//        System.out.println(response.toString());
+//        
+//        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
+//
+//        System.out.println(securityResponse.toString());
+//
+//        if(securityResponse.getSuccess()){
+//            return Response
+//                    .ok()
+//                    .build();
+//        }else{
+//            return Response
+//                    .status(response.getStatus())
+//                    .entity(securityResponse)
+//                    .build();
+//        }
+//    }
+//
+//    @POST
+//    @Path("login")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    public Response login(MultivaluedMap<String, String> formParams) {
+//        WebTarget resourceWebTarget = webTarget.path("login");
+//        Response response = resourceWebTarget
+//                .request(MediaType.APPLICATION_XML_TYPE)
+//                .post(Entity.form(formParams));
+//
+//        response.bufferEntity();
+//        
+//        System.out.println(response.toString());
+//        
+//        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
+//        
+//        if(securityResponse.getSuccess()){
+//	        return Response
+//	                .ok()
+//	                .cookie(new NewCookie("authToken", securityResponse.getAuthToken()))
+//	                .build();
+//	    }else{
+//	    	return Response
+//	                .status(response.getStatus())
+//	                .entity(securityResponse)
+//	                .build();
+//	    }
+//    }
+//
+//    @POST
+//    @Path("logout")
+//    public Response logout(@CookieParam("authToken") String authToken) {
+//        System.out.println("@CookieParam: " + authToken);
+//        Form form = new Form();
+//        form.param("authToken", authToken);
+//
+//        WebTarget resourceWebTarget = webTarget.path("logout");
+//        Response response = resourceWebTarget
+//                .request(MediaType.APPLICATION_XML_TYPE)
+//                .post(Entity.form(form));
+//
+//        response.bufferEntity();
+//        
+//        System.out.println(response.toString());
+//        
+//        SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
+//        
+//        if(securityResponse.getSuccess()){
+//	        return Response
+//	                .ok()
+//	                .header("Set-Cookie",
+//                            "authToken=deleted;Expires=Thu, 01-Jan-1970 00:00:01 GMT")
+//	                .build();
+//        }else{
+//        	return Response
+//	                .status(response.getStatus())
+//                    .entity(securityResponse)
+//	                .build();
+//        }
+//    }
+//
+//    @POST
+//    @Path("isloggedin")
+//    public Response isLoggedIn(@CookieParam("authToken") String authToken) {
+//        SecurityResponse securityResponse;
+//
+//        if (authToken == null) {
+//            securityResponse = new SecurityResponse(false, "No token");
+//            return Response
+//                    .ok()
+//                    .entity(securityResponse)
+//                    .build();
+//        } else {
+//            System.out.println("@CookieParam: " + authToken);
+//            Form form = new Form();
+//            form.param("authToken", authToken);
+//
+//            WebTarget resourceWebTarget = webTarget.path("isvalidtoken");
+//            Response response = resourceWebTarget
+//                    .request(MediaType.APPLICATION_XML_TYPE)
+//                    .post(Entity.form(form));
+//
+//            response.bufferEntity();
+//
+//            System.out.println(response.toString());
+//        
+//            securityResponse = response.readEntity(SecurityResponse.class);
+//
+//
+//            if(securityResponse.getSuccess()){
+//                return Response
+//                        .ok()
+//                        .build();
+//            } else {
+//                return Response
+//                        .status(response.getStatus())
+//                        .entity(securityResponse)
+//                        .build();
+//            }
+//        }
+//    }
 
     @POST
     @Path("activateuser")
-    public String activateUser() {
-        return "{\"API\": \"activateUser working\"}";
+    public Response activateUser() throws SOAPException, JAXBException {
+         
+//         WebTarget resourceWebTarget = webTarget_SOAP.path("registeruser");
+         
+    	 ClientConfig config = new DefaultClientConfig();
+         config.getClasses().add(SoapProvider.class);
+         Client c = Client.create(config);
+         c.addFilter(new LoggingFilter());
+         
+         MessageFactory messageFactory = MessageFactory.newInstance();
+         SOAPMessage message = messageFactory.createMessage();
+         SOAPPart soapPart = message.getSOAPPart();
+         SOAPEnvelope envelope = soapPart.getEnvelope();
+         SOAPBody body = envelope.getBody();
+        
+         SOAPElement bodyElement = body.addChildElement(envelope.createName("registerUser", "", securityApiUrl_SOAP));
+         
+         bodyElement.addChildElement("formParams").addTextNode("59102");
+         
+         message.saveChanges();
+         
+         WebResource service = c.resource("http://ws.cdyne.com/WeatherWS/Weather.asmx");
+         
+         // POST the request
+         ClientResponse cr = service.header("SOAPAction", "\"http://ws.cdyne.com/WeatherWS/GetCityWeatherByZIP\"").post(ClientResponse.class, message);
+         message = cr.getEntity(SOAPMessage.class);
+  
+         JAXBContext ctx = JAXBContext.newInstance(SecurityResponse.class);
+         Unmarshaller um = ctx.createUnmarshaller();
+         SecurityResponse response = (um.unmarshal(message.getSOAPPart().getEnvelope().getBody().extractContentAsDocument(), SecurityResponse.class)).getValue();
+         System.out.println("City : " + response.getReason());
+         System.out.println("Temperature : " + response.toString());
+//         
+//         WebResource service = client_SOAP.resource("http://ws.cdyne.com/WeatherWS/Weather.asmx");
+//         
+//         Response response = resourceWebTarget
+// 				                .request(MediaType.APPLICATION_XML_TYPE)
+// 				                .post(Entity.form(formParams));
+//         
+//         response.bufferEntity();
+//
+//         System.out.println(response.toString());
+//         
+//         SecurityResponse securityResponse = response.readEntity(SecurityResponse.class);
+//
+//         System.out.println(securityResponse.toString());
+//
+//         if(securityResponse.getSuccess()){
+             return Response
+                     .ok()
+                     .build();
+//         }else{
+//             return Response
+//                     .status(response.getStatus())
+//                     .entity(securityResponse)
+//                     .build();
+//         }
+    	
+       // return "{\"API\": \"activateUser working\"}";
     }
 
     @POST
