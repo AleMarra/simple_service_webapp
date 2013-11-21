0565d9a803a63c9588969db72ed6b2c37d1d1207
AleMarra <alemarra@gmail.com>
2013-11-21 07:36:31 -0300

    Changed url patterns

--- src/main/java/com/fiuba/taller/service/SecurityService.java
+++ src/main/java/com/fiuba/taller/service/SecurityService.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-@Path("/")
+@Path("/securityservice")
 @Produces(MediaType.APPLICATION_JSON)
 public class SecurityService {
 
