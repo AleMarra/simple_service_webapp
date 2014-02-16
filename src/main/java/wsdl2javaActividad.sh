f028f45f4f43ed155f6ba4432090c8b0df9593b1
sebastian.bogado <sebastian.bogado@fdvsolutions.com>
2014-02-16 14:08:35 -0300

    -noBuildXML param added to wsdl2java script

--- src/main/java/wsdl2javaActividad.sh
+++ src/main/java/wsdl2javaActividad.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm wtp/activity/fiuba/taller/actividad/Actividad*
-wsdl2java.sh -S wtp/activity -R wtp/activity -uri Actividad.wsdl
+wsdl2java.sh --noBuildXML -S wtp/activity -R wtp/activity -uri Actividad.wsdl
 mv -v build.xml wtp/activity/
 cd wtp/activity/fiuba/taller/actividad/
 sed -i 's/package fiuba\.taller\.actividad;/package wtp\.activity\.fiuba\.taller\.actividad;/g' Actividad*
